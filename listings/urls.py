425ae588535119c6efab07482225190db8cc2bcb
asishkr123 <asishkrramuk@gmail.com>
2019-02-16 15:28:20 +0530

    search added

--- listings/urls.py
+++ listings/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 
 
-
 from . import views
 
 
 urlpatterns = [
-    path('<int:listing_id>' , views.listing , name = "listing"),
-    path('' , views.index , name = 'listings'),
-    path('search' , views.search , name = 'search'),
-
+    path("<int:listing_id>", views.listing, name="listing"),
+    path("", views.index, name="listings"),
+    path("search", views.search, name="search"),
 ]
+
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+
+from .models import Realtor
+
 # Register your models here.
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "email", "hire_date")
+    list_dispaly_links = ("id", "name")
+    search_fields = ("name",)
+    list_per_page = 25
+
+admin.site.register(Realtor, RealtorAdmin)
+
